8dad59ccc340e6d52948ab9ababd0ff8d1a02903
TheFatTony <anton.alexeyev@outlook.com>
2018-02-07 17:28:38 +0300

    exchanges-info tests implemented

--- src/main/java/com/chieftain/agency/dao/tickerdata/TickerDataDao.java
+++
@@ -1,9 +0,0 @@
-package com.chieftain.agency.dao.tickerdata;
-
-import com.chieftain.agency.dao.Dao;
-import com.chieftain.agency.entity.TickerData;
-
-public interface TickerDataDao extends Dao<TickerData, Long> {
-
-
-}
